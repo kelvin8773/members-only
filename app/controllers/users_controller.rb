aefb0cca8cf41414312dbf95a8bd261eede44de7
bright okike <brightokike@gmail.com>
2019-08-14 13:17:28 +0300

    sign up UI added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
+  def new
+    @page_title = 'Add User'
+    @user = User.new
+  end
+
+  def create
+     @user = User.new(user_params)
+     if @user.save
+      flash[:notice] = 'User created'
+      redirect_to root_url
+     else 
+      render 'new'
+     end
+  end
 
   def show
     if logged_in?
       @user = current_user
     end
   end
+
+  private 
+    def  user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
+  get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
-  resources :users, only: [:show]
+  resources :users
   resources :posts, only: [:new, :create, :index]
 end
